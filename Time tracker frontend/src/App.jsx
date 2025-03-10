f31c56acf5b434c85ed69922f8a2bb54af2776a8
Manthan Patel <manthannpatel0603@gmail.com>
2025-03-10 23:54:28 +0530

    Updatedcss

--- Time tracker frontend/src/App.jsx
+++ Time tracker frontend/src/App.jsx
@@ -7,7 +7,8 @@ import { DeveloperDashboard } from "./components/Developer/DeveloperDashboard";
 import { ProjectManagerDashboard } from "./components/ProjectManager/ProjectManagerDashboard";
 import { AddProject } from "./components/Admin/AddProject";
 import ProjectPage from "./components/Admin/Project";
-import Tasks from "../src/components/Admin/Tasks"; // Import Tasks component
+import Tasks from "./components/Admin/Tasks"; // Import Tasks component
+import Reports from "./components/Admin/Reports"; // Import Reports component
 import axios from "axios";
 import "./css/styles.css";
 
@@ -27,6 +28,8 @@ function App() {
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
           <Route path="/admin/addProject" element={<AddProject />} />
           <Route path="/admin/projects" element={<ProjectPage />} />
+          <Route path="/admin/tasks" element={<Tasks />} />
+          <Route path="/admin/reports" element={<Reports />} /> {/* New Reports Route */}
           
           {/* Developer Routes */}
           <Route path="/developer" element={<Navigate to="/developer/dashboard" />} />
@@ -35,9 +38,6 @@ function App() {
           {/* Project Manager Routes */}
           <Route path="/ProjectManager" element={<Navigate to="/ProjectManager/dashboard" />} />
           <Route path="/ProjectManager/dashboard" element={<ProjectManagerDashboard />} />
-          
-          {/* New Route for Tasks Page */}
-          <Route path="/admin/tasks" element={<Tasks />} />
         </Routes>
       </div>
     </Router>
